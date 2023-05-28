a7a3da30ef44617bf7ce7a32a40c86750f937686
Геннадий <@DESKTOP-6NN51IE>
2023-05-28 12:19:57 +0300

    1

--- func++/Source.cpp
+++ func++/Source.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 
 void dos(int number1, int number2) {
-	int i, sum = 0, k;
+
+	int i, sum = 0, k, j;
 	for (i = number1; i <= number2; i++)
-		for (k = 1; k < i; k++) {
-			if (i % k == 0) {
-				sum += k;
-				if (sum == i) {
-					cout << i << " ";
-					sum = 0;
-				}
+		for (k = 1, j = 1; k < i; j++, k++) {
+			if (i % j == 0) {
+				sum += j;
+			}
+			if (sum == i) {
+				cout << i << " ";
+				sum = 0;
 			}
 		}
-	k = 1;
 }
 
 int main() {
@@ -27,6 +27,6 @@ int main() {
 
 	dos(number1,number2);
 
-	system("pause>nul");
+	//system("pause>nul");
 	return 0;
 }
