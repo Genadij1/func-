12d7bb91f9cab659a3ba9fe9900a69bd3d9a0239
Геннадий <@DESKTOP-6NN51IE>
2023-05-30 13:38:23 +0300

    ex_4-done

--- func++/Source.cpp
+++ func++/Source.cpp
@@ -1,24 +1,15 @@
 #include <iostream>
-#include <cstdlib>
+#include <string>
 using namespace std;
 
-void dos(int number) {
-	int i, sum = 0;
-	for (i = 1; i < number; i++)
-		if (number % i == 0) {
-			sum += i;
-		}
-	cout << sum;
+
+void displayCard(string card) {
+    cout << "card: " << card <<endl;
 }
 
 int main() {
+    string card = "Jack of Crosses";
+    displayCard(card);
 
-	int number;
-	cout << "Enter the number: ";
-	cin >> number;
-
-	dos(number);
-
-	system("pause>nul");
-	return 0;
+    return 0;
 }
